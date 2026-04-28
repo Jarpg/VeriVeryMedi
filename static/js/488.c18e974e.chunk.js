b5f03e83b143e4650094b5957cb84240aad022c8
Jarpg <115106777+Jarpg@users.noreply.github.com>
2026-04-27 22:01:53 -0400

    Delete static directory

--- static/js/488.c18e974e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(globalThis.webpackChunkverifayada=globalThis.webpackChunkverifayada||[]).push([[488],{488(a,e,h){new WeakMap;new Set}}]);
-//# sourceMappingURL=488.c18e974e.chunk.js.map
